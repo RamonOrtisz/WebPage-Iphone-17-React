4df0770f06de224613a0f276f6e1a51a57496538
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-11-10 19:10:41 -0300

    intro creating

---
+++ src/components/Intro.jsx
@@ -0,0 +1,28 @@
+function Intro(){
+
+    const specs = [
+        { value: '6.3', label: 'Display Super Retina XDR', color: 'text-blue-600'},
+        { value: 'A18 Pro', label: 'Chip mais rápido', color: 'text-orange-500'},
+        { value: '48MP', label: 'Sistema de câmera', color: 'text-blue-600'},
+        { value: '29h', label: 'Bateria', color: 'text-orange-500'},
+    ]
+
+    return (
+        <section className="bg-black py-20 px-6">
+            <div className="max-w-5xl mx-auto text-center">
+                <h1 className="text-6xl md:text-8xl font-bold mb-6">IPhone 17 Pro</h1>
+                <p className="text-2xl md:text-4xl mb-4 text-gradient font-bold">Titânio. Tão forte. Tão leve. Tão Pro.</p>
+                <p className="text-lg md:text-xl text-gray-300 mb-10 max-w-3xl mx-auto">O design mais refinado que já criamos. titânio de grau aeroespacial. Chip A18 Pro. Sistema de câmera Pro revolucionário.</p>
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-4 justify-center items-center mb-16 font-medium">
+                <button className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-full text-lg font-medium transition-all duration-300 hover:scale-105 shadow-lg shadow-blue-500 cursor-pointer
+                /50">Compre agora</button>
+                <button>Saiba mais</button>
+            </div>
+
+        </section>
+    )
+}
+
+export default Intro
