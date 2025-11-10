cf8723a4525c3d8f196156e269f47835aacca232
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-11-10 18:31:59 -0300

    final hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,29 @@
 function Hero() {
-  return (
-    <section className="relative h-screen overflow-hidden bg-black">
-      <div className="absolute top-16 left-0 bottom-0 z-0">
-        <img src="../../public/img/hero.jpg" alt="" />
-      </div>
+    return (
+        <section className="relative h-screen overflow-hidden bg-black">
+            <div className="absolute top-19 bottom-0 z-0">
+                <img src="/img/hero.jpg" className="w-full h-full object-cover opacity-90" alt="" />
+                <div className="absolute inset-0 bg-gradient-to-b from-black/30 via-transparent to-black/80"></div>
+            </div>
 
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-          />
-        </svg>
-      </div>
-    </section>
-  );
+            <div className="absolute bottom-8 left-1/2 z-10 animate-bounce">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2.5}
+                    stroke="currentColor"
+                    className="size-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+                    />
+                </svg>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
